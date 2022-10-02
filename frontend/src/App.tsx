a85e57fecf4ca8c0691c2261e9456f1c5a70987d
Alper <as.alper1@hotmail.com>
2022-10-02 13:28:21 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,15 @@
-import { useEffect, useState } from "react";
-import { PicAPI } from "./Api/PicReqs";
-import { PicDto } from "./Dto/picDto";
+import Categories from "./Categories";
+import Grid from "./Grid";
+import Header from "./Header";
 
 function App() {
-  const [respPics, setRespPics] = useState<PicDto[]>([]);
 
-  const fetchAndSetPics = async () => {
-    setRespPics(await PicAPI.getAllPics());
-  };
-
-  useEffect(() => {
-    fetchAndSetPics();
-  }, []);
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      {respPics.map((pic, picIndex) => (
-        <div key={picIndex}>
-          <h1>{pic.title}</h1>
-          <img
-            src={`data:${pic.picture_file.contentType};base64,${pic.picture_file.data}`}
-            alt=""
-          />
-        </div>
-      ))}
+      <Header/>
+      <Categories/>
+      <Grid/>
     </div>
   );
 }
