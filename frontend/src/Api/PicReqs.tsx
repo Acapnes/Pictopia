8c08ab71b7bd713cc9387d885bac5eb10e37cafb
Alper <as.alper1@hotmail.com>
2022-10-04 21:30:02 +0300

    Details Req

--- frontend/src/Api/PicReqs.tsx
+++ frontend/src/Api/PicReqs.tsx
@@ -10,4 +10,14 @@ export class PicAPI {
 
     return data;
   }
+
+  public static async getDetailPic(_id: string): Promise<PicDto> {
+    const resp = await fetch(`http://localhost:3000/pics/${_id}`, {
+      method: "GET",
+    });
+
+    const data = await resp.json();
+
+    return data;
+  }
 }
