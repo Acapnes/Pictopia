6c6fcede02220f4281d448f470144f31f0f4eb99
Alper <as.alper1@hotmail.com>
2022-10-10 10:30:41 +0300

    Create jwt.strategy.ts

---
+++ backend/src/helpers/AuthGuards/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'super-jwt-secret-key',
+    });
+  }
+
+  async validate(payload: any) {
+    return payload;
+  }
+}
