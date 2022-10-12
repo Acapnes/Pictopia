21c4218f3c5fe1b46be93a92ad02fd6b640c2b81
Alper <as.alper1@hotmail.com>
2022-10-12 17:38:27 +0300

    new buttons

--- frontend/src/components/PrettyButtons.tsx
+++ frontend/src/components/PrettyButtons.tsx
@@ -109,4 +109,95 @@ const PrettyRegister = () => {
   );
 };
 
-export { PrettyShare, PrettySave, PrettyComments, PrettyReport, PrettyLogin, PrettyRegister };
+const PrettySaveChanges = () => {
+  return (
+    <div className="relative h-fit w-fit p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+      <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+      <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+        <span className="text-white">Save</span>
+      </span>
+    </div>
+  );
+};
+
+const PrettyChangeProfileAvatar = () => {
+  return (
+    <div className="relative h-fit w-fit p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+      <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+      <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+        <span className="text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            fill="currentColor"
+            className="bi bi-camera-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+            <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0z" />
+          </svg>
+        </span>
+      </span>
+    </div>
+  );
+};
+
+const PrettyExtendedChangeProfile = () => {
+  return (
+    <div className="relative px-2 py-2 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group text-sm w-fit">
+      <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+      <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+      <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+      <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+      <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease space-y-1">
+        <p>Extended Menu</p>
+        <div className="w-full flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            fill="currentColor"
+            className="bi bi-arrow-bar-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"
+            />
+          </svg>
+        </div>
+      </span>
+    </div>
+  );
+};
+
+const PrettyHeaderSignIn = () => {
+  return (
+    <a
+      href="/login"
+      className="w-[6rem] text-sm relative inline-flex items-center justify-start px-2 py-2 overflow-hidden font-bold rounded-full group"
+    >
+      <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+      <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
+      <span className="relative w-full text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900 text-center">
+        SIGN IN
+      </span>
+      <span className="absolute inset-0 border-2 border-white rounded-full"></span>
+    </a>
+  );
+};
+
+export {
+  PrettyShare,
+  PrettySave,
+  PrettyComments,
+  PrettyReport,
+  PrettyLogin,
+  PrettyRegister,
+  PrettySaveChanges,
+  PrettyChangeProfileAvatar,
+  PrettyExtendedChangeProfile,
+  PrettyHeaderSignIn,
+};
