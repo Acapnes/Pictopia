7c258b69eb1eb489b0130458cb02b2069d3ab09c
Alper <as.alper1@hotmail.com>
2022-10-12 17:38:34 +0300

    new icons

--- frontend/src/components/PrettyIcons.tsx
+++ frontend/src/components/PrettyIcons.tsx
@@ -30,4 +30,38 @@ const PrettyEyeIcon = (props: any) => {
   );
 };
 
-export { PrettyEyeIcon };
+const PrettyOptionsIcon = (props: any) => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-three-dots-vertical"
+        viewBox="0 0 16 16"
+      >
+        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+      </svg>
+    </div>
+  );
+};
+
+const PrettySearchIcon = (props: any) => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="gray"
+        className="bi bi-search"
+        viewBox="0 0 16 16"
+      >
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+      </svg>
+    </div>
+  );
+};
+
+export { PrettyEyeIcon, PrettyOptionsIcon, PrettySearchIcon };
