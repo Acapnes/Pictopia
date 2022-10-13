9162fab3ac1df2b3fd508724f0f0ed182b4f3008
Alper <as.alper1@hotmail.com>
2022-10-13 20:09:55 +0300

    exception handling v1

--- backend/src/models/user/auth.service.ts
+++ backend/src/models/user/auth.service.ts
@@ -1,4 +1,4 @@
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from 'src/schemas/user.schema';
@@ -7,19 +7,19 @@ import { UserRegistrationDto } from 'src/dto/user/user.registration.dto';
 import { UserService } from './user.service';
 import { UserValidationDto } from 'src/dto/user/user.validation.dto';
 import { ReturnAuthDto } from 'src/dto/returns/return.auth.dto';
+import { UserDto } from 'src/dto/user/user.dto';
+import { UserCredentialsDto } from 'src/dto/user/user.credentials.dto';
+import { ReturnFuncDto } from 'src/dto/returns/return.func.dto';
 
 @Injectable()
-export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>,private userService: UserService) {}
+export class AuthService {constructor(@InjectModel(User.name) private userModel: Model<UserDocument>,private userService: UserService) {}
 
   async createUser(userRegistrationDto: UserRegistrationDto): Promise<ReturnAuthDto> {
-    const checkEmail = await this.userService.findByEmail(
-      userRegistrationDto.email,
-    );
+    const checkEmail = await this.userService.findByEmail(userRegistrationDto.email);
     if (checkEmail != null)
       return {
         access: false,
-        access_token:"",
+        access_token: '',
         message: 'This email already been used',
       };
 
@@ -30,30 +30,52 @@ export class AuthService {
     this.userModel.create(userRegistrationDto);
     return {
       access: true,
-      access_token:"",
+      access_token: '',
       message: 'User has been registered',
     };
   }
 
   async validateLoginUser(userValidationDto: UserValidationDto): Promise<ReturnAuthDto> {
-    const selectedUser = await this.userService.findByEmail(userValidationDto.email);
-    const rawPassword = userValidationDto.password.toString();
-    const loginResult = bcrypt.compareSync(
-      rawPassword,
-      selectedUser.password.toString(),
-    );
-    if (loginResult) {
-      return {
-        access: true,
-        message: 'Access verification successful',
-        access_token: await this.userService.generateLoginToken(selectedUser),
-      };
-    } else {
-      return {
-        access: false,
-        message: 'Access verification failed',
-        access_token: '',
-      };
-    }
+    return await this.userService.findByEmail(userValidationDto.email).then(async (findReturn : any) => {
+        if (findReturn.success !== false) {
+          const selectedUser = findReturn as UserDto;
+          const rawPassword = userValidationDto.password.toString();
+          const loginResult = bcrypt.compareSync(rawPassword,selectedUser.password.toString());
+          if (loginResult) {
+            return {
+              access: true,
+              message: 'Access verification successful',
+              access_token: await this.userService.generateLoginToken(selectedUser._id),
+            };
+          } else {
+            return {
+              access: false,
+              message: 'Access verification failed',
+              access_token: '',
+            };
+          }
+        }
+        return findReturn;
+      });
+  }
+
+  async fetchUserCredentialsWithToken(_id: mongoose.Types.ObjectId,): Promise<UserCredentialsDto>{
+    return await this.userService.findByMongooseId(_id).then(async (funcResult: any) => {
+        if (funcResult.success !== false) {
+          return {
+            name: funcResult.name,
+            email: funcResult.email,
+            username: funcResult.username,
+            avatar: {
+              data: funcResult.avatar.data,
+              contentType: funcResult.avatar.contentType,
+            },
+            birthDate: funcResult.birthDate,
+            confrimed: funcResult.confrimed,
+            bio: funcResult.bio,
+          };
+        }
+        return funcResult;
+      });
   }
 }
--- backend/src/models/user/moderation.service.ts
+++ backend/src/models/user/moderation.service.ts
@@ -1,43 +1,49 @@
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from 'src/schemas/user.schema';
 import { UserService } from './user.service';
 import { ReturnAuthDto } from 'src/dto/returns/return.auth.dto';
 import { UserUpdateDto } from 'src/dto/user/user.update.dto';
+import { ReturnFuncDto } from 'src/dto/returns/return.func.dto';
+import { UserDto } from 'src/dto/user/user.dto';
 
 @Injectable()
 export class ModerationService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
-    private userService: UserService,
-  ) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>,private userService: UserService) {}
 
-  async updateProfile(userUpdateDto: UserUpdateDto): Promise<ReturnAuthDto> {
-    if (!(await this.userService.findByMongooseId(userUpdateDto._id)))
-      return {
-        access: false,
-        access_token: null,
-        message: 'User cannot found!',
-      };
-
-    return await this.userModel
-      .findOneAndUpdate(
-        { _id: userUpdateDto._id },
-        {
-          name: userUpdateDto.name,
-          username: userUpdateDto.username,
-          avatar: userUpdateDto.avatar,
-          bio: userUpdateDto.bio,
-          birthdate: userUpdateDto.birthDate,
-        },
-      )
-      .then(async () => {
-        return {
-          access: true,
-          message: 'Your profile has been changed!',
-          access_token: await this.userService.generateLoginToken(userUpdateDto),
-        };
+  async updateProfile(_id: mongoose.Types.ObjectId | any,avatar_file: any,userUpdateDto: UserUpdateDto,): Promise<ReturnAuthDto> {
+    return await this.userService.findByMongooseId(_id).then(async (funcResult: any) => {
+        if (funcResult.success !== false) {
+          return await this.userModel.findOneAndUpdate(
+              { _id: _id },
+              {
+                name: userUpdateDto.name,
+                username: userUpdateDto.username,
+                avatar: {
+                  data: avatar_file.buffer,
+                  contentType: avatar_file.mimetype,
+                },
+                bio: userUpdateDto.bio,
+                birthdate: userUpdateDto.birthDate,
+              },
+            )
+            .then(async () => {
+              return {
+                access: true,
+                message: 'Your profile has been changed!',
+                access_token: await this.userService.generateLoginToken(_id),
+              };
+            })
+            .catch((err) => {
+              return {
+                access: false,
+                message: 'Something went wrong! : ' + err,
+                access_token: '',
+              };
+            });
+        }
+        return funcResult;
       });
   }
 }
