a87b1d1122dd9e823f26e366b335e86e1dfcb712
Alper <as.alper1@hotmail.com>
2022-10-16 18:44:58 +0300

    Full push 2

---
+++ frontend/src/Grids/ActionlessGrid.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { PicDto } from "../Api/PicDtos/picDto";
+import { PicAPI } from "../Api/PicApi";
+
+const ActionlessGrid = () => {
+  const [respPics, setRespPics] = useState<PicDto[]>([]);
+
+  const fetchAndSetPics = async () => {
+    setRespPics(await PicAPI.getAllPics());
+  };
+
+  useEffect(() => {
+    fetchAndSetPics();
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      <div className="columns-1 sm:columns-2 md:columns-2 lg:columns-3 xl:columns-4 2xl:columns-4 3xl:columns-5 4xl:colmuns-6 gap-4 z-0 w-fit space-y-4 ">
+        {respPics.map((pic, picIndex) => (
+          <div
+            className="group relative h-fit w-full flex flex-col justify-center items-center-4 "
+            key={picIndex}
+          >
+            <img
+              src={`data:${pic.picture_file.contentType};base64,${pic.picture_file.data}`}
+              alt=""
+              className="min-w-full"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ActionlessGrid;
---
+++ frontend/src/Menus/Header.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserAPI } from "../Api/UserApi";
+import { UserDto } from "../Api/UserDtos/userDto";
+import Categories from "./components/Categories";
+import {
+  PrettyCategories,
+  PrettyHeaderOptions,
+  PrettyHeaderSignIn,
+  PrettySearch,
+  PrettyUploadPicture,
+} from "../components/PrettyButtons";
+import { PrettyOptionsIcon, PrettyPictopia, PrettyProfileIcon } from "../components/PrettyIcons";
+
+const Header = () => {
+  const [showCategories, setShowCategories] = useState(false);
+  const [showUploadMenu, setShowUploadMenu] = useState(false);
+  const [userCredentials, setUserCredentials] = useState<UserDto>(Object);
+
+  const initFetchCredentials = async () => {
+    const access_token = window.localStorage.getItem("access_token") as string;
+    setUserCredentials(await UserAPI.fetchUserCredentials(access_token));
+  };
+  useEffect(() => {
+    initFetchCredentials();
+  }, []);
+
+  return (
+    <div className="w-full mb-3 z-10 space-y-3 sticky top-0">
+      <div className="w-full h-[5.2rem] flex flex-row justify-between space-x-4 px-5 bg-soft-black bg-opacity-90 py-2">
+        <div className="flex flex-row space-x-2">
+          <Link
+            to={"/"}
+            className="h-full rounded-md my-[0.5rem] min-w-[10rem]"
+          >
+            <PrettyPictopia />
+          </Link>
+          <button
+            onClick={() => setShowCategories(!showCategories)}
+            className="flex items-center"
+          >
+            <PrettyCategories showCategories={showCategories} />
+          </button>
+        </div>
+
+        <div className="w-full hidden lg:flex lg:items-center ">
+          <PrettySearch />
+        </div>
+        <div className="flex flex-row space-x-2">
+          <button
+            onClick={() => setShowUploadMenu(!showUploadMenu)}
+            className="flex items-center"
+          >
+            <PrettyUploadPicture showUploadMenu={showUploadMenu} />
+          </button>
+          {window.localStorage.getItem("access_token") ? (
+            <a
+              href="http://localhost:3001/user"
+              className="flex bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] rounded-full min-w-[4rem]"
+            >
+              <PrettyProfileIcon user={userCredentials} />
+            </a>
+          ) : (
+            <PrettyHeaderSignIn />
+          )}
+          <button><PrettyHeaderOptions/></button>
+        </div>
+      </div>
+      <div className={`${showCategories ? "block" : "hidden"}`}>
+        <Categories />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
