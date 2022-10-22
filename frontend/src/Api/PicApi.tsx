18c28c07a6cc88fbbff768e5368b63ab3d652fa8
Alper <as.alper1@hotmail.com>
2022-10-22 19:42:30 +0300

    upload picture req

--- frontend/src/Api/PicApi.tsx
+++ frontend/src/Api/PicApi.tsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { CommentDto } from "./PicDtos/commentDto";
 import { PicDto } from "./PicDtos/picDto";
+import { UploadPicDto } from "./PicDtos/uploadPicDto";
 
 export class PicAPI {
   public static async getAllPics(): Promise<PicDto[]> {
@@ -31,5 +33,15 @@ export class PicAPI {
     const data = await resp.json();
 
     return data;
+  }
+
+  public static async uploadPicture(uploadPicDto:UploadPicDto, access_token: string){
+    const formData = new FormData();
+    formData.append("picture", uploadPicDto.picture);
+    formData.append("title", uploadPicDto.title);
+    formData.append("description", uploadPicDto.description);
+
+    axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`
+    await axios.post(`http://localhost:3000/pics/create/`,formData).then(resp => console.log(resp))
   }
 }
