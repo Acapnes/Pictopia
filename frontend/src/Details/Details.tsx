371eb505c6072fadbe8be9c771075062864a1e55
Alper <as.alper1@hotmail.com>
2022-10-26 17:06:54 +0300

    .

--- frontend/src/Details/Details.tsx
+++ frontend/src/Details/Details.tsx
@@ -4,6 +4,7 @@ import { PicAPI } from "../Api/PicApi";
 import Grid from "../Grids/Grid";
 import Header from "../Menus/Header";
 import PictureDetailsCard from "./components/PictureDetailsCard";
+import { PrettyErrorIcon } from "../components/PrettyIcons";
 
 const Details = () => {
   const urlParam =
@@ -21,14 +22,26 @@ const Details = () => {
   return (
     <div className="w-full h-full flex flex-col bg-soft-black">
       <Header />
-      <div className="min-h-[70vh] flex flex-col  justify-center items-center py-10 3xl:flex-row 3xl:px-10 3xl:space-x-5 lg:px-3 ">
-        <div className="bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] rounded-sm shadow-lg p-[0.2rem] w-fit h-fit mb-10 relative">
-          <img
-            src={`data:${picture?.picture_file?.contentType};base64,${picture?.picture_file?.data}`}
-            alt=""
-            className="object-contain max-h-[70vh] max-w-[60vw] rounded-sm"
-          />
-        </div>
+      <div className="min-h-[70vh] flex flex-col  justify-center items-center px-2 py-10 3xl:flex-row 3xl:px-10 3xl:space-x-5 lg:px-3 ">
+        {picture?.picture_file?.data || !picture?.picture_file?.contentType ? (
+          <div className="bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] rounded-sm shadow-lg p-[0.2rem] w-fit h-fit mb-10 bg-red-500 relative">
+            <img
+              src={`data:${picture?.picture_file?.contentType};base64,${picture?.picture_file?.data}`}
+              alt=""
+              className="object-contain max-h-[70vh] 3xl:max-w-[55vw] rounded-sm"
+            />
+          </div>
+        ) : (
+          <div className="bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] rounded-sm shadow-lg p-[0.2rem] w-fit h-fit mb-10 relative">
+            <div className="w-full h-full bg-soft-black items-center space-y-5 p-5 flex flex-col">
+              <div className="text-gray-200 text-2xl font-semibold">
+                Picture not found
+              </div>
+              <PrettyErrorIcon size={30} fill={"rgb(244,114,182)"} />
+            </div>
+          </div>
+        )}
+
         <PictureDetailsCard picture={picture} />
       </div>
       <div className="w-full lg:col-span-2 md:col-span-2 ">
--- frontend/src/components/CustomAlert.tsx
+++ frontend/src/components/CustomAlert.tsx
@@ -1,13 +1,34 @@
+import { useEffect } from "react";
 import { PrettyAlertIcon } from "./PrettyIcons";
 
 const CustomAlert = (props: any) => {
+
+  useEffect(() => {
+    setTimeout(()=>{
+      console.log(props?.result?.message)
+    },2000)
+  }, []);
+
+
   return (
-    <div className={`${props.result.message ? "block" : "hidden"} bg-gray-800 py-3 px-4`}>
-      <div className="w-full h-full text-white flex flex-row space-x-2 break-all">
-        <PrettyAlertIcon size={26} />
-        <p className={`w-full h-full text-center space-x-2 font-semibold ${props?.result?.access | props?.result?.success ? "text-green-400":"text-red-400"}`}>
-          {props.result.message}
-        </p>
+    <div className="">
+      <div
+        className={`${props.result.message ? "block" : "hidden"} ${
+          props.background === true && "bg-gray-800"
+        } py-4 px-4`}
+      >
+        <div className="w-full h-full text-white flex flex-row space-x-2 break-all">
+          <PrettyAlertIcon size={26} />
+          <p
+            className={`w-full h-full text-center space-x-2 font-semibold ${
+              props?.result?.access | props?.result?.success
+                ? "text-green-400"
+                : "text-red-400"
+            }`}
+          >
+            {props.result.message}
+          </p>
+        </div>
       </div>
     </div>
   );
