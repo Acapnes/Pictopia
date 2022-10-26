b30280234059c2aa6b210e567e4e05e84062551d
Alper <as.alper1@hotmail.com>
2022-10-26 17:04:50 +0300

    return to axios

---
+++ frontend/src/Api/CommentApi.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { CommentCreateDto } from "./PicDtos/commentCreateDto";
+import { CommentDto } from "./PicDtos/commentDto";
+import { BackendReturnFuncDto } from "./Utils/backend.return.func.dto";
+
+export class CommentAPI {
+  public static async getCommentsOfPicture(_id: string): Promise<CommentDto[]> {
+    return await axios
+      .get(`http://localhost:3000/comments/${_id}`)
+      .then((resp) => resp.data);
+  }
+
+  public static async postCommentToPicture(access_token: string,commentCreateDto: CommentCreateDto): Promise<BackendReturnFuncDto> {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+    return await axios.post("http://localhost:3000/comments/create", commentCreateDto).then((resp) => resp.data);
+  }
+}
--- frontend/src/Api/PicApi.tsx
+++ frontend/src/Api/PicApi.tsx
@@ -1,55 +1,31 @@
 import axios from "axios";
-import { CommentDto } from "./PicDtos/commentDto";
 import { PicDto } from "./PicDtos/picDto";
 import { UploadPicDto } from "./PicDtos/uploadPicDto";
 
 export class PicAPI {
   public static async getAllPics(): Promise<PicDto[]> {
-    const resp = await fetch("http://localhost:3000/pics", {
-      method: "GET",
-    });
-
-    const data = await resp.json();
-
-    return data;
+    return await axios.get("http://localhost:3000/pics")
+      .then((resp) => resp.data);
   }
 
   public static async getDetailPic(_id: string): Promise<PicDto> {
-    const resp = await fetch(`http://localhost:3000/pics/${_id}`, {
-      method: "GET",
-    });
-
-    const data = await resp.json();
-
-    return data;
+    return await axios.get(`http://localhost:3000/pics/${_id}`)
+      .then((resp) => resp.data);
   }
 
-  public static async getCommentsOfPicture(_id: string): Promise<CommentDto[]> {
-    const resp = await fetch(`http://localhost:3000/comments/${_id}`, {
-      method: "GET",
-      headers: { "Content-type": "application/json" },
-    });
-
-    const data = await resp.json();
-
-    return data;
-  }
-
-  public static async uploadPicture(uploadPicDto:UploadPicDto, access_token: string){
-
-    console.log(uploadPicDto.picture)
-    if(!uploadPicDto.title){
-      return{
-        success:false,
-        message:"Please set a title"
-      }
+  public static async uploadPicture(uploadPicDto: UploadPicDto, access_token: string) {
+    if (!uploadPicDto.title) {
+      return {
+        success: false,
+        message: "Please set a title",
+      };
     }
 
-    if(!uploadPicDto?.picture?.size){
-      return{
-        success:false,
-        message:"Please select a picture"
-      }
+    if (!uploadPicDto?.picture?.size) {
+      return {
+        success: false,
+        message: "Please select a picture",
+      };
     }
 
     const formData = new FormData();
@@ -57,7 +33,8 @@ export class PicAPI {
     formData.append("title", uploadPicDto.title);
     formData.append("description", uploadPicDto.description);
 
-    axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`
-    return await axios.post(`http://localhost:3000/pics/create/`,formData).then(resp => resp.data)
+    axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+    return await axios.post(`http://localhost:3000/pics/create/`, formData)
+      .then((resp) => resp.data);
   }
 }
