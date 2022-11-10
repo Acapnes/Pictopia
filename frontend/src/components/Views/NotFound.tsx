5a1051dd7a7ee68a4a5b17f1e03681bbbb2be8d8
Alper <as.alper1@hotmail.com>
2022-11-11 02:45:04 +0300

    not found page

--- frontend/src/components/Views/NotFound.tsx
+++ frontend/src/components/Views/NotFound.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { PrettyBugIcon } from "../Prettys/PrettyIcons";
 
 const Notfound = () => {
   return (
-    <div>Notfound</div>
-  )
-}
+    <div className="flex flex-col h-fit w-fit items-center mt-20">
+      <div className="w-full">
+        <p className="text-[8rem] md:text-[10rem] 3xl:text-[12rem] text-gray-200 text-center">
+          404
+        </p>
+      </div>
+      <div className="w-full h-full flex flex-row space-x-5 items-center">
+        <PrettyBugIcon size={40} fill={"#f472b6"} />
+        <div className="flex flex-col text-center">
+          <p className="text-xl md:text-2xl 3xl:text-3xl text-gray-200">
+            Something went wrong
+          </p>
+          <p className="text-xl md:text-2xl 3xl:text-3xl text-gray-200">
+            Please try again later
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Notfound
+export default Notfound;
