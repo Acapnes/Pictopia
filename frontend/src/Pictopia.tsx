2d45ba9bad62a803e0d281792f139bd87ff0154a
Alper <as.alper1@hotmail.com>
2022-11-22 00:14:01 +0300

    .

--- frontend/src/Pictopia.tsx
+++ frontend/src/Pictopia.tsx
@@ -1,6 +1,6 @@
 import Grid from "./Picture/Grids/Grid";
 import Header from "./Menus/Header";
-
+  
 function App() {
   return (
     <div className="min-h-screen flex flex-col space-y-3 bg-soft-black">
